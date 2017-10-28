60dc126232691a429d074e739e5a619791395c42
Jerry Webster <jerweb63@gmail.com>
2017-10-28 21:42:10 +0200

    Implemented placement of computer's ships on grid.

--- src/battleship/BattleShipsGame.java
+++ src/battleship/BattleShipsGame.java
@@ -1,41 +1,96 @@
 package battleship;
+import java.util.Random;
 import java.util.Scanner;
 
 public class BattleShipsGame {
 
     private static String header = "    0123456789";
     private static String footer = "    0123456789";
-    private static char[][] oceanMap = new char[10][10];
+    private static int[][] map = new int[10][10];
 
     public static void main(String[] args) {
         System.out.println("**** Welcome to the Battleships Game ****\n\n");
 
+        int i = 0;
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter X coordinate for your ship: ");
-        int x = input.nextInt();
-        System.out.print("Enter Y coordinate for your ship: ");
-        int y = input.nextInt();
+
+        while (i < 5) {
+            System.out.print("Enter X coordinate for your ship: ");
+            int x = input.nextInt();
+            System.out.print("Enter Y coordinate for your ship: ");
+            int y = input.nextInt();
+
+            // The player should not enter a coordinate that's already in use.
+            if (map[x][y] == 1) {
+                System.out.println("\nCoordinate has already been used.");
+                continue;
+            } else {
+                map[x][y] = 1;
+            }
+            i++;
+        }
+
         System.out.println();
-        seaMap();
+
+        oceanMap();
     }
 
-    public static void seaMap() {
-        System.out.println(header);
+    // Prints the grid to the screen.
+    public static void oceanMap() {
 
+        System.out.println(header);
         int i;
         int j;
 
-        for (i = 0; i < oceanMap.length; i++) {
+        for (i = 0; i < map.length; i++) {
             System.out.print(" " + i + " |");
 
-            for (j = 0; j < oceanMap[i].length; j++) {
-                oceanMap[i][j] = ' ';
-                System.out.print(oceanMap[i][j]);
+            for (j = 0; j < map[i].length; j++) {
+                if (map[i][j] == 0) {
+                    System.out.print(" ");
+                } else if (map[i][j] == 1){
+                    System.out.print("@");
+                }
             }
             System.out.print("| " + i);
             System.out.println();
         }
 
-        System.out.println(footer);
+        System.out.println(footer + "\n");
+        deployShips();
+    }
+
+    // Make random numbers for the computer's grid coordinates
+    private static int genCoordinate(){
+        // Upper and lower bounds for computer's coordinates
+        int min = 0;
+        int max = 9;
+        int number;
+
+        Random rand = new Random();
+        return number = rand.nextInt(max - min + 1) + min;
+    }
+
+    // Place computer's ships on the grid.
+    private static void deployShips() {
+
+        System.out.println("Computer is deploying ships");
+        int i = 0;
+        int x, y;
+
+        while (i < 5) {
+            x = genCoordinate();
+            y = genCoordinate();
+
+            if (map[x][y] == 0) {
+                map[x][y] = 2;
+                System.out.println(i + 1 + ". ship DEPLOYED");
+                i++;
+            } else {
+                continue;
+            }
+        }
+
+        System.out.println(i + 1 + "----------------------");
     }
 }
